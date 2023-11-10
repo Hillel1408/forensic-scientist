1cb691e35c977e65aa35c156bac5338861cb2c6f
Hillel1408 <etemax@bk.ru>
2023-11-10 13:05:04 +0300

    Судмедэксперт

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,3 +1,11 @@
+const introSwiperThumbs = new Swiper(".intro__swiper-thumbs", {
+  enabled: false,
+  breakpoints: {
+    1240: {
+      enabled: true,
+    },
+  },
+});
 const introSwiper = new Swiper(".intro__swiper", {
   direction: "horizontal",
   slidesPerView: "auto",
@@ -8,5 +16,14 @@ const introSwiper = new Swiper(".intro__swiper", {
   pagination: {
     el: ".intro__swiper-pagination",
     clickable: true,
+  },
+  thumbs: {
+    swiper: introSwiperThumbs,
+  },
+  enabled: false,
+  breakpoints: {
+    1240: {
+      enabled: true,
+    },
   },
 });
